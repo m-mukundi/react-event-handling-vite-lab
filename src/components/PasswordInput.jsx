6fd74693b3a4191159bcef1aa933e26e954fa050
Mercy Mukundi <mercy.n.mukundi@gmail.com>
2026-05-30 00:23:08 +0300

    Event Listeners Added

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,9 +1,35 @@
 // Code PasswordInput Component Here
 
-function PasswordInput (){
-    return(
-        <></>
-    )
+function PasswordInput() {
+	function handleChange() {
+		console.log("Entering password...");
+	}
+
+	function handleEnter() {
+		console.log("Mouse Entering");
+	}
+
+	function handleLeave() {
+		console.log("Mouse Leaving");
+	}
+
+	return (
+		<>
+			<form>
+				<label>Password: </label>
+				<input type="password" onChange={() => handleChange()}></input>
+				<br />
+				<br />
+				<button
+					type="submit"
+					onMouseEnter={() => handleEnter()}
+					onMouseLeave={() => handleLeave()}
+				>
+					Submit Password
+				</button>
+			</form>
+		</>
+	);
 }
 
 export default PasswordInput;
